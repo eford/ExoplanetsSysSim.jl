76f43d3c3b248b3d66f93723a96b27896aecef47
Eric Ford <eford@psu.edu>
2017-06-30 12:15:59 -0400

    README for downloading stellar tables

---
+++ data/download_stellar_tables.sh
@@ -0,0 +1,5 @@
+wget "http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=q1_q17_dr24_stellar&format=csv&select=*"
+mv nph-nstedAPI\?table=q1_q17_dr24_stellar\&format=csv\&select=\* q1_q17_dr24_stellar.csv
+wget "http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=q1_q17_dr25_stellar&format=csv&select=*"
+mv nph-nstedAPI\?table=q1_q17_dr25_stellar\&format=csv\&select=\* q1_q17_dr25_stellar.csv
+
