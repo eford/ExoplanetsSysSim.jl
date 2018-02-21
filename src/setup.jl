92ec240f5938c84e4bade8f209c227082c831724
Eric Ford <eford@psu.edu>
2018-02-21 13:00:19 -0500

    fixing deprecations

--- src/setup.jl
+++ src/setup.jl
@@ -9,17 +9,28 @@ end
 startdir = pwd()
 # Code to be run once to install unregistered package dependencies
 # Code to be run once to install non-standard dependencies
-Pkg.clone("git@github.com:eford/ABC.jl.git")
-# switch to dragozzine's fork for any minor SysSim specific ABC changes
-
-Pkg.clone("git@github.com:jbrakensiek/CORBITS.git")
-
-# Compile CORBITS library and put it somewhere we can find
-cd(joinpath(Pkg.dir(),"CORBITS"))
-run(`make lib`)
-cd(homedir())
-if !is_windows()
-   symlink( joinpath(Pkg.dir("CORBITS"),"libcorbits.so"), joinpath(Pkg.dir("ExoplanetsSysSim"),"libcorbits.so") )
-else
-   cp( joinpath(Pkg.dir("CORBITS"),"libcorbits.so"), joinpath(Pkg.dir("ExoplanetsSysSim"),"libcorbits.so") )
+try
+  Pkg.clone("git@github.com:eford/ABC.jl.git")
+  # switch to dragozzine's fork for any minor SysSim specific ABC changes
+catch
+  warn("Attempted to install ABC.jl package, but was not successful.")
+  warn("While most of SysSim will still work, some functionality will not be avaliable unless you install CORBITS correctly.")
 end
+
+try
+  Pkg.clone("git@github.com:jbrakensiek/CORBITSa.git")
+
+  # Compile CORBITS library and put it somewhere we can find
+  cd(joinpath(Pkg.dir(),"CORBITS"))
+  run(`make lib`)
+  cd(homedir())
+  if !is_windows()
+     symlink( joinpath(Pkg.dir("CORBITS"),"libcorbits.so"), joinpath(Pkg.dir("ExoplanetsSysSim"),"libcorbits.so") )
+  else
+     cp( joinpath(Pkg.dir("CORBITS"),"libcorbits.so"), joinpath(Pkg.dir("ExoplanetsSysSim"),"libcorbits.so") )
+  end
+catch
+  warn("Attempted to install CORBITS.jl package, but was not successful.")
+  warn("While most of SysSim will still work, some functionality will not be avaliable unless you install CORBITS correctly.")
+end
+
