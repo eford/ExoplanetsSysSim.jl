3ff9374dfa81ba65fe4217427f6ce6910f984158
Matthias Yang He <myh7@psu.edu>
2018-03-16 17:12:01 -0400

    Updated optimize.jl to use weighted sum of distances and added a new model parameter max_incl_sys

--- examples/clusters/generate_systems.jl
+++ examples/clusters/generate_systems.jl
@@ -3,11 +3,14 @@ sim_param = setup_sim_param_model()
 
 
 
-tic()
+
 
 ##### To generate the underlying systems:
+tic()
 
 cat_phys = generate_kepler_physical_catalog(sim_param)
+
+toc()
 
 ##### For saving the underlying/true planets/systems:
 
@@ -63,9 +66,32 @@ for (i,targ) in enumerate(cat_phys.target)
 end
 close(f)
 
+##### For saving the stellar properties of all the systems:
+
+f = open("stellar_masses_with_planets.out", "w")
+write_model_params(f, sim_param)
+for (i,targ) in enumerate(cat_phys.target)
+    if length(targ.sys[1].planet) > 0
+        star_mass = targ.sys[1].star.mass #solar masses
+        println(f, star_mass)
+    end
+end
+close(f)
+
+f = open("stellar_radii_with_planets.out", "w")
+write_model_params(f, sim_param)
+for (i,targ) in enumerate(cat_phys.target)
+    if length(targ.sys[1].planet) > 0
+        star_radius = targ.sys[1].star.radius #solar radii
+        println(f, star_radius)
+    end
+end
+close(f)
+
 
 
 ##### To generate the observed systems:
+tic()
 
 cat_phys_cut = ExoplanetsSysSim.generate_obs_targets(cat_phys,sim_param)
 cat_obs = observe_kepler_targets_single_obs(cat_phys_cut,sim_param)
@@ -78,62 +104,92 @@ toc()
 f = open("periods.out", "w")
 write_model_params(f, sim_param)
 for num_pl_in_sys in 1:length(summary_stat.cache["idx_n_tranets"])
-  num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
-  period_array = Array{Float64}(num_pl_in_sys,num_targets)
-  for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
-      period_array[:,i] = map(ExoplanetsSysSim.period, cat_obs.target[j].obs)[1:num_pl_in_sys]
-  end
-  println(f,"# Periods of systems with ", num_pl_in_sys, " detected planets.")
-  if length(period_array) > 0
-    println(f,period_array')
-  end
+    num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+    period_array = Array{Float64}(num_pl_in_sys,num_targets)
+    for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+        period_array[:,i] = map(ExoplanetsSysSim.period, cat_obs.target[j].obs)[1:num_pl_in_sys]
+    end
+    println(f,"# Periods of systems with ", num_pl_in_sys, " detected planets.")
+    if length(period_array) > 0
+        println(f,period_array')
+    end
 end
 close(f)
 
 f = open("depths.out", "w")
 write_model_params(f, sim_param)
 for num_pl_in_sys in 1:length(summary_stat.cache["idx_n_tranets"])
-  num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
-  depths_array = Array{Float64}(num_pl_in_sys,num_targets)
-  for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
-      depths_array[:,i] = map(ExoplanetsSysSim.depth, cat_obs.target[j].obs)[1:num_pl_in_sys]
-  end
-  println(f,"# Transit depths of systems with ", num_pl_in_sys, " detected planets.")
-  if length(depths_array) > 0
-    println(f,depths_array')
-  end
+    num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+    depths_array = Array{Float64}(num_pl_in_sys,num_targets)
+    for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+        depths_array[:,i] = map(ExoplanetsSysSim.depth, cat_obs.target[j].obs)[1:num_pl_in_sys]
+    end
+    println(f,"# Transit depths of systems with ", num_pl_in_sys, " detected planets.")
+    if length(depths_array) > 0
+        println(f,depths_array')
+    end
 end
 close(f)
 
 f = open("durations.out", "w")
 write_model_params(f, sim_param)
 for num_pl_in_sys in 1:length(summary_stat.cache["idx_n_tranets"])
-  num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
-  duration_array = Array{Float64}(num_pl_in_sys,num_targets)
-  for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
-      duration_array[:,i] = map(ExoplanetsSysSim.duration, cat_obs.target[j].obs)[1:num_pl_in_sys]
-  end
-  println(f,"# Transit durations of systems with ", num_pl_in_sys, " detected planets.")
-  if length(duration_array) > 0
-    println(f,duration_array')
-  end
+    num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+    duration_array = Array{Float64}(num_pl_in_sys,num_targets)
+    for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+        duration_array[:,i] = map(ExoplanetsSysSim.duration, cat_obs.target[j].obs)[1:num_pl_in_sys]
+    end
+    println(f,"# Transit durations of systems with ", num_pl_in_sys, " detected planets.")
+    if length(duration_array) > 0
+        println(f,duration_array')
+    end
 end
 close(f)
-
 
 #=
 f = open("eccentricities.out", "w")
 for num_pl_in_sys in 1:length(summary_stat.cache["idx_n_tranets"])
-  num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
-  ecc_array = Array{Float64}(num_pl_in_sys,num_targets)
-  for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
-      ecc_array[:,i] = map(ExoplanetsSysSim.eccentricity, cat_obs.target[j].obs)[1:num_pl_in_sys]
-  end
-  println(f,"# Eccentricities of systems with ", num_pl_in_sys, " detected planets.")
-  if length(ecc_array) > 0
-    println(f,ecc_array')
-  end
+    num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+    ecc_array = Array{Float64}(num_pl_in_sys,num_targets)
+    for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+        ecc_array[:,i] = map(ExoplanetsSysSim.eccentricity, cat_obs.target[j].obs)[1:num_pl_in_sys]
+    end
+    println(f,"# Eccentricities of systems with ", num_pl_in_sys, " detected planets.")
+    if length(ecc_array) > 0
+        println(f,ecc_array')
+    end
 end
 close(f)
 =#
 
+##### For saving the stellar properties of the systems with observed planets:
+
+f = open("stellar_masses_obs.out", "w")
+write_model_params(f, sim_param)
+for num_pl_in_sys in 1:length(summary_stat.cache["idx_n_tranets"])
+    num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+    stellar_mass_array = Array{Float64}(num_targets)
+    for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+        stellar_mass_array[i] = cat_obs.target[j].star.mass
+    end
+    println(f,"# Stellar masses of systems with ", num_pl_in_sys, " detected planets.")
+    if length(stellar_mass_array) > 0
+        println(f, stellar_mass_array)
+    end
+end
+close(f)
+
+f = open("stellar_radii_obs.out", "w")
+write_model_params(f, sim_param)
+for num_pl_in_sys in 1:length(summary_stat.cache["idx_n_tranets"])
+    num_targets = length(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+    stellar_radius_array = Array{Float64}(num_targets)
+    for (i,j) in enumerate(summary_stat.cache["idx_n_tranets"][num_pl_in_sys])
+        stellar_radius_array[i] = cat_obs.target[j].star.radius
+    end
+    println(f,"# Stellar radii of systems with ", num_pl_in_sys, " detected planets.")
+    if length(stellar_radius_array) > 0
+        println(f, stellar_radius_array)
+    end
+end
+close(f)
--- examples/clusters/param_common.jl
+++ examples/clusters/param_common.jl
@@ -12,50 +12,52 @@ function setup_sim_param_model(args::Vector{String} = Array{String}(0) )   # all
   # For generating target star properties
   add_param_fixed(sim_param,"generate_kepler_target",generate_kepler_target_from_table)
   add_param_fixed(sim_param,"star_table_setup",setup_star_table_christiansen)
-#add_param_fixed(sim_param,"stellar_catalog","q1_q17_dr24_stellar.jld")
+  #add_param_fixed(sim_param,"stellar_catalog","q1_q17_dr24_stellar.jld")
   add_param_fixed(sim_param,"stellar_catalog","q1_q17_dr24_stellar.csv")
-  # add_param_fixed(sim_param,"generate_kepler_target",ExoplanetsSysSim.generate_kepler_target_simple)  # An alternative that alternative can be used for testing if above breaks
+  #add_param_fixed(sim_param,"generate_kepler_target",ExoplanetsSysSim.generate_kepler_target_simple)  # An alternative that alternative can be used for testing if above breaks
 
   # For generating planetary system properties
   add_param_fixed(sim_param,"generate_planetary_system", generate_planetary_system_clustered)
 
   add_param_fixed(sim_param,"generate_num_clusters",generate_num_clusters_poisson) 
   add_param_fixed(sim_param,"generate_num_planets_in_cluster",generate_num_planets_in_cluster_poisson)
-  add_param_fixed(sim_param,"log_rate_clusters",log(2.49))
+  add_param_active(sim_param,"log_rate_clusters",log(2.49))
   add_param_fixed(sim_param,"max_clusters_in_sys",10)
-  add_param_fixed(sim_param,"log_rate_planets_per_cluster",log(2.92))
+  add_param_active(sim_param,"log_rate_planets_per_cluster",log(2.92))
   add_param_fixed(sim_param,"max_planets_in_cluster",10)
 
   # generate_num_planets_in_cluster currently calls: generate_periods_power_law
   add_param_fixed(sim_param,"generate_sizes",ExoplanetsSysSim.generate_sizes_broken_power_law) # To choose the way we draw planetary radii; if "generate_sizes_power_law", then takes "power_law_r"; if "generate_sizes_broken_power_law", then takes "power_law_r1", "power_law_r2", and "break_radius"
-  add_param_fixed(sim_param,"power_law_P",-0.060)
+  add_param_active(sim_param,"power_law_P",-0.060)
   add_param_fixed(sim_param,"power_law_r",-2.5)
-  add_param_fixed(sim_param,"power_law_r1",-1.83)
-  add_param_fixed(sim_param,"power_law_r2",-4.07)
-  add_param_fixed(sim_param,"min_period",5.0)
+  add_param_active(sim_param,"power_law_r1",-1.83)
+  add_param_active(sim_param,"power_law_r2",-4.07)
+  add_param_fixed(sim_param,"min_period",3.0)
   add_param_fixed(sim_param,"max_period",300.0)
   add_param_fixed(sim_param,"min_radius",0.5*ExoplanetsSysSim.earth_radius)
   add_param_fixed(sim_param,"max_radius",10.*ExoplanetsSysSim.earth_radius)
-  add_param_fixed(sim_param,"break_radius",4.23*ExoplanetsSysSim.earth_radius)
+  add_param_active(sim_param,"break_radius",4.23*ExoplanetsSysSim.earth_radius)
 
   # generate_num_planets_in_cluster currently use these for the Inclination distribution
-  add_param_fixed(sim_param,"sigma_incl",1.57) # degrees; 0 = coplanar w/ generate_kepler_target_simple; ignored by generate_planetary_system_uncorrelated_incl
-  add_param_fixed(sim_param,"sigma_incl_near_mmr",0.99)
+  add_param_active(sim_param,"sigma_incl",1.57) # degrees; 0 = coplanar w/ generate_kepler_target_simple; ignored by generate_planetary_system_uncorrelated_incl
+  add_param_active(sim_param,"sigma_incl_near_mmr",0.99)
+
+  add_param_fixed(sim_param,"max_incl_sys",0.0) #degrees; gives system inclinations from "max_incl_sys" (deg) to 90 (deg), so set to 0 for isotropic distribution of system inclinations; NOTE: make sure the difference between this and 90 (deg) is at least greater than "sigma_incl" and "sigma_incl_near_mmr"!
 
   # generate_num_planets_in_cluster currently use these for the Eccentricity distribution
   add_param_fixed(sim_param,"generate_e_omega",ExoplanetsSysSim.generate_e_omega_rayleigh)
-  add_param_fixed(sim_param,"sigma_hk",0.048)
+  add_param_active(sim_param,"sigma_hk",0.048)
   #add_param_fixed(sim_param,"sigma_hk_one",0.1)
   #add_param_fixed(sim_param,"sigma_hk_multi",0.03)
 
   # generate_num_planets_in_cluster currently use these for the Stability tests
-  add_param_fixed(sim_param,"num_mutual_hill_radii",13.12)
+  add_param_active(sim_param,"num_mutual_hill_radii",13.12)
   add_param_fixed(sim_param,"generate_planet_mass_from_radius",ExoplanetsSysSim.generate_planet_mass_from_radius_powerlaw)
-  add_param_fixed(sim_param,"mr_power_index",2.42)
+  add_param_active(sim_param,"mr_power_index",2.42)
   add_param_fixed(sim_param,"mr_const",1.0)
   add_param_fixed(sim_param,"mr_max_mass",1e3*ExoplanetsSysSim.earth_mass)
   add_param_active(sim_param,"sigma_log_radius_in_cluster",0.25)
-  add_param_fixed(sim_param,"sigma_logperiod_per_pl_in_cluster",0.12)
+  add_param_active(sim_param,"sigma_logperiod_per_pl_in_cluster",0.12)
 
   # Functions to calculate observables from physical system properties
   add_param_fixed(sim_param,"calc_target_obs_single_obs",ExoplanetsSysSim.calc_target_obs_single_obs)   
@@ -85,11 +87,14 @@ end
 function write_model_params(f, sim_param::SimParam)
     #This function writes all the model parameters to a file f as a header
     println(f, "# num_targets_sim_pass_one: ", get_int(sim_param,"num_targets_sim_pass_one"))
+    println(f, "# max_incl_sys: ", get_real(sim_param,"max_incl_sys"))
     println(f, "# log_rate_clusters: ", get_real(sim_param,"log_rate_clusters"))
     println(f, "# max_clusters_in_sys: ", get_int(sim_param,"max_clusters_in_sys"))
     println(f, "# log_rate_planets_per_cluster: ", get_real(sim_param,"log_rate_planets_per_cluster"))
     println(f, "# max_planets_in_clusters: ", get_int(sim_param,"max_planets_in_cluster"))
     println(f, "# power_law_P: ", get_real(sim_param,"power_law_P"))
+    println(f, "# min_period: ", get_real(sim_param,"min_period"))
+    println(f, "# max_period: ", get_real(sim_param,"max_period"))
 #println(f, "# power_law_r: ", get_real(sim_param,"power_law_r"))
     println(f, "# power_law_r1: ", get_real(sim_param,"power_law_r1"))
     println(f, "# power_law_r2: ", get_real(sim_param,"power_law_r2"))
