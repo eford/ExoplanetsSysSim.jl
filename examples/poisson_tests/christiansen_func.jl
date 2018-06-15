39240b8a961994ce0893d6eae6bfa82d8da48f2b
dch216 <dch216@psu.edu>
2018-06-14 20:18:19 -0400

    Changed stellar radius used in sum. stats

--- examples/poisson_tests/christiansen_func.jl
+++ examples/poisson_tests/christiansen_func.jl
@@ -356,7 +356,8 @@ function calc_summary_stats_obs_binned_rates(cat_obs::KeplerObsCatalog, param::S
       for j in 1:num_planets(cat_obs.target[i])
         period_list[n] = cat_obs.target[i].obs[j].period
         weight_list[n] = ExoplanetsSysSim.prob_detect(cat_obs.target[i].prob_detect,j)
-        radius_list[n] = sqrt(cat_obs.target[i].obs[j].depth)*cat_obs.target[i].star.radius
+        #radius_list[n] = sqrt(cat_obs.target[i].obs[j].depth)*cat_obs.target[i].star.radius
+        radius_list[n] = sqrt(cat_obs.target[i].obs[j].depth)*star_table(cat_obs.target[i].star.id, :radius)
         n = n+1
       end
     end
@@ -365,7 +366,8 @@ function calc_summary_stats_obs_binned_rates(cat_obs::KeplerObsCatalog, param::S
       for j in 1:num_planets(cat_obs.target[i])
         period_list[n] = cat_obs.target[i].obs[j].period
         weight_list[n] = 1.0
-        radius_list[n] = sqrt(cat_obs.target[i].obs[j].depth)*cat_obs.target[i].star.radius
+        #radius_list[n] = sqrt(cat_obs.target[i].obs[j].depth)*cat_obs.target[i].star.radius
+        radius_list[n] = sqrt(cat_obs.target[i].obs[j].depth)*star_table(cat_obs.target[i].star.id, :radius)
         n = n+1
       end
     end
