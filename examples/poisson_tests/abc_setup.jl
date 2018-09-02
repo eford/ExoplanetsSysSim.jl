dd7642a9f8cac90246d974d38a3a39ada68c2a26
dch216 <dch216@psu.edu>
2018-09-02 11:58:21 -0400

    Use Beta dist for proposal

--- examples/poisson_tests/abc_setup.jl
+++ examples/poisson_tests/abc_setup.jl
@@ -3,67 +3,106 @@
 # Collection of functions which specific ABC simulation parameters
 
 module EvalSysSimModel
-  export setup, get_param_vector, get_ss_obs #, evaluate_model
-  export gen_data, calc_summary_stats, calc_distance, is_valid, normalize_dirch
-  using ExoplanetsSysSim
-  include(joinpath(Pkg.dir(),"ExoplanetsSysSim","examples","poisson_tests", "christiansen_func.jl"))
+export setup, get_param_vector, get_ss_obs #, evaluate_model
+export gen_data, calc_summary_stats, calc_distance, is_valid, normalize_dirch
+export make_proposal_dist_multidim_beta
+using ExoplanetsSysSim
+include(joinpath(Pkg.dir(),"ExoplanetsSysSim","examples","poisson_tests", "christiansen_func.jl"))
 
-  sim_param_closure = SimParam()
-  summary_stat_ref_closure =  CatalogSummaryStatistics()
+sim_param_closure = SimParam()
+summary_stat_ref_closure =  CatalogSummaryStatistics()
 
-    function is_valid(param_vector::Vector{Float64})
-      global sim_param_closure
-      update_sim_param_from_vector!(param_vector,sim_param_closure)
-      const rate_tab::Array{Float64,2} = get_any(sim_param_closure, "obs_par", Array{Float64,2})
-      #const lambda = sum_kbn(rate_tab)
-      if any(x -> x < 0., rate_tab[2:size(rate_tab,1),:]) || any(x -> x > 3., rate_tab[1,:])# || lambda > 10.
-         return false
-      end
-      return true
+function is_valid(param_vector::Vector{Float64})
+    global sim_param_closure
+    update_sim_param_from_vector!(param_vector,sim_param_closure)
+    const rate_tab::Array{Float64,2} = get_any(sim_param_closure, "obs_par", Array{Float64,2})
+    #const lambda = sum_kbn(rate_tab)
+    if any(x -> x < 0., rate_tab[2:size(rate_tab,1),:]) || any(x -> x > 3., rate_tab[1,:])# || lambda > 10.
+        return false
+    end
+    return true
+end
+
+function normalize_dirch(param_vector::Vector{Float64})
+    global sim_param_closure
+    const p_dim = length(get_any(sim_param_closure, "p_lim_arr", Array{Float64,1}))-1
+    const r_dim = length(get_any(sim_param_closure, "r_lim_arr", Array{Float64,1}))-1
+    
+    for i in 1:p_dim
+        param_vector[((i-1)*(r_dim+1)+2):((i-1)*(r_dim+1)+(r_dim+1))] ./= sum(param_vector[((i-1)*(r_dim+1)+2):((i-1)*(r_dim+1)+(r_dim+1))])
     end
 
-    function normalize_dirch(param_vector::Vector{Float64})
-        global sim_param_closure
-        const p_dim = length(get_any(sim_param_closure, "p_lim_arr", Array{Float64,1}))-1
-        const r_dim = length(get_any(sim_param_closure, "r_lim_arr", Array{Float64,1}))-1
+    update_sim_param_from_vector!(param_vector,sim_param_closure)
+    return param_vector
+end
 
-        for i=1:p_dim
-            param_vector[((i-1)*(r_dim+1)+2):((i-1)*(r_dim+1)+(r_dim+1))] ./= sum(param_vector[((i-1)*(r_dim+1)+2):((i-1)*(r_dim+1)+(r_dim+1))])
-        end
+function make_proposal_dist_multidim_beta(theta::AbstractArray{Float64,2}, weights::AbstractArray{Float64,1},  tau_factor::Float64; verbose::Bool = false)
+    global sim_param_closure
+    const p_dim = length(get_any(sim_param_closure, "p_lim_arr", Array{Float64,1}))-1
+    const r_dim = length(get_any(sim_param_closure, "r_lim_arr", Array{Float64,1}))-1
+    const max_col_rate = 3.0
+    
+    theta_mean =  sum(theta.*weights',dims=2) # weighted mean for parameters
+    tau = tau_factor*ABC.var_weighted(theta'.-theta_mean',weights)  # scaled, weighted covar for parameters
 
-        update_sim_param_from_vector!(param_vector,sim_param_closure)
-        return param_vector
+    function alpha(x_bar::T, v_bar::T) where T<: Real 
+        x_bar * (((x_bar * (1 - x_bar)) / v_bar) - 1)
+    end
+    function beta(x_bar::T, v_bar::T) where T<: Real 
+        (1 - x_bar) * (((x_bar * (1 - x_bar)) / v_bar) - 1)
+    end
+    
+    #=
+    println("mean= ",theta_mean)
+    println("var= ",tau)
+    for i in 1:length(theta_mean)
+        println("a= ",alpha(theta_mean[i],tau[i]), "  b= ",beta(theta_mean[i],tau[i]))
+    end
+    =#
+
+    dist_arr = ContinuousDistribution[]
+    for j in 1:p_dim
+        col_startidx = (j-1)*(r_dim+1)+1
+        dist_arr = vcat(dist_arr, LinearTransformedBeta(alpha(theta_mean[col_startidx]/max_col_rate,tau[col_startidx]/max_col_rate^2), beta(theta_mean[col_startidx]/max_col_rate,tau[col_startidx]/max_col_rate^2),xmin=0.0,xmax=max_col_rate), 
+             ContinuousDistribution[ Beta(alpha(theta_mean[i],tau[i]), beta(theta_mean[i],tau[i])) for i in (col_startidx+1):(col_startidx+r_dim)]   )
     end
 
-    function gen_data(param_vector::Vector{Float64})
-      global sim_param_closure
-      update_sim_param_from_vector!(param_vector,sim_param_closure)
-      cat_phys = generate_kepler_physical_catalog(sim_param_closure)
-      #cat_phys_cut = ExoplanetsSysSim.generate_obs_targets(cat_phys, sim_param_closure)
-      #cat_obs = ExoplanetsSysSim.observe_kepler_targets_single_obs(cat_phys_cut, sim_param_closure)
-      cat_obs = ExoplanetsSysSim.observe_kepler_targets_sky_avg(cat_phys, sim_param_closure)
-      return cat_obs
-    end
+    dist = ABC.CompositeDistributions.CompositeDist()
+end
 
-    # TODO OPT: Eventually, could adapt ABC.jl to use distance from first pass to decide if should compute additional summary statistics
-    function calc_summary_stats(cat::KeplerObsCatalog)
-      global sim_param_closure
-      sum_stat = calc_summary_stats_obs_binned_rates(cat, sim_param_closure, obs_skyavg = true)
-      return sum_stat
-    end
+function make_proposal_dist_multidim_beta(pop::abc_population_type, tau_factor::Float64; verbose::Bool = false)
+    make_proposal_dist_multidim_beta(pop.theta, pop.weights, tau_factor, verbose=verbose)
+end
 
-    function calc_distance(sum_stat_obs::CatalogSummaryStatistics,sum_stat_sim::CatalogSummaryStatistics, n::Integer = 0)
-      global sim_param_closure
-      dist1 = calc_distance_vector_binned(sum_stat_obs,sum_stat_sim, 1, sim_param_closure)
-      num_available = length(dist1)
-      num_to_use = n>0 ? min(n,num_available) : num_available
-      return calc_scalar_distance(dist1[1:num_to_use])
-    end
+function gen_data(param_vector::Vector{Float64})
+    global sim_param_closure
+    update_sim_param_from_vector!(param_vector,sim_param_closure)
+    cat_phys = generate_kepler_physical_catalog(sim_param_closure)
+    #cat_phys_cut = ExoplanetsSysSim.generate_obs_targets(cat_phys, sim_param_closure)
+    #cat_obs = ExoplanetsSysSim.observe_kepler_targets_single_obs(cat_phys_cut, sim_param_closure)
+    cat_obs = ExoplanetsSysSim.observe_kepler_targets_sky_avg(cat_phys, sim_param_closure)
+    return cat_obs
+end
 
-  function setup()
+# TODO OPT: Eventually, could adapt ABC.jl to use distance from first pass to decide if should compute additional summary statistics
+function calc_summary_stats(cat::KeplerObsCatalog)
+    global sim_param_closure
+    sum_stat = calc_summary_stats_obs_binned_rates(cat, sim_param_closure, obs_skyavg = true)
+    return sum_stat
+end
+
+function calc_distance(sum_stat_obs::CatalogSummaryStatistics,sum_stat_sim::CatalogSummaryStatistics, n::Integer = 0)
+    global sim_param_closure
+    dist1 = calc_distance_vector_binned(sum_stat_obs,sum_stat_sim, 1, sim_param_closure)
+    num_available = length(dist1)
+    num_to_use = n>0 ? min(n,num_available) : num_available
+    return calc_scalar_distance(dist1[1:num_to_use])
+end
+
+function setup()
     global sim_param_closure = setup_sim_param_christiansen()
     sim_param_closure = set_test_param(sim_param_closure)
-
+    
     ### Use simulated planet candidate catalog data
     #add_param_fixed(sim_param_closure,"num_kepler_targets",150000)  # For "observed" catalog
     #cat_obs = simulated_read_kepler_observations(sim_param_closure)
@@ -76,33 +115,33 @@ module EvalSysSimModel
     println("# Finished reading in KOI data")  
     cat_obs = setup_actual_planet_candidate_catalog(df_star, df_koi, usable_koi, sim_param_closure)
     ###
-
+    
     global summary_stat_ref_closure = calc_summary_stats_obs_binned_rates(cat_obs,sim_param_closure, trueobs_cat = true)
-  end
+end
 
-  get_param_vector() = make_vector_of_sim_param(sim_param_closure)
-  get_ss_obs() = summary_stat_ref_closure
+get_param_vector() = make_vector_of_sim_param(sim_param_closure)
+get_ss_obs() = summary_stat_ref_closure
 
-  function set_simparam_ss(sim_param::ExoplanetsSysSim.SimParam, ss_true::ExoplanetsSysSim.CatalogSummaryStatistics)
+function set_simparam_ss(sim_param::ExoplanetsSysSim.SimParam, ss_true::ExoplanetsSysSim.CatalogSummaryStatistics)
     global sim_param_closure = sim_param
     global summary_stat_ref_closure = ss_true
-  end
+end
 
 end  # module EvalSysSimModel
 
 include(joinpath(Pkg.dir("ABC"),"src/composite.jl"))
 
 module SysSimABC
-  export setup_abc, run_abc, run_abc_largegen
-  import ABC
-  import Distributions
-  using CompositeDistributions
-  using Compat
-  import ExoplanetsSysSim
-  import EvalSysSimModel
-  include(joinpath(Pkg.dir(),"ExoplanetsSysSim","examples","poisson_tests", "christiansen_func.jl"))
+export setup_abc, run_abc, run_abc_largegen
+import ABC
+import Distributions
+using CompositeDistributions
+using Compat
+import ExoplanetsSysSim
+import EvalSysSimModel
+include(joinpath(Pkg.dir(),"ExoplanetsSysSim","examples","poisson_tests", "christiansen_func.jl"))
 
-  function setup_abc(num_dist::Integer = 0)
+function setup_abc(num_dist::Integer = 0)
     EvalSysSimModel.setup()
     theta_true = EvalSysSimModel.get_param_vector()
     #param_prior = CompositeDist( Distributions.ContinuousDistribution[Distributions.Uniform(0., 0.3) for x in 1:length(theta_true)] )
@@ -110,42 +149,42 @@ module SysSimABC
     const r_dim = length(get_any(EvalSysSimModel.sim_param_closure, "r_lim_arr", Array{Float64,1}))-1
     prior_arr = ContinuousDistribution[]
     for i in 1:(length(limitP)-1)
-      max_in_col = 3*log(limitP[i+1]/limitP[i])/log(2)
-      lambda_col = Uniform(0.0, max_in_col)
-      dirch = Dirichlet(ones(r_dim))
-      prior_arr = vcat(prior_arr, [lambda_col, dirch])
+        max_in_col = 3*log(limitP[i+1]/limitP[i])/log(2)
+        lambda_col = Uniform(0.0, max_in_col)
+        dirch = Dirichlet(ones(r_dim))
+        prior_arr = vcat(prior_arr, [lambda_col, dirch])
     end
     param_prior = CompositeDist(prior_arr)
     in_parallel = nworkers() > 1 ? true : false
-
+    
     calc_distance_ltd(sum_stat_obs::ExoplanetsSysSim.CatalogSummaryStatistics,sum_stat_sim::ExoplanetsSysSim.CatalogSummaryStatistics) = EvalSysSimModel.calc_distance(sum_stat_obs,sum_stat_sim,num_dist)
+    
+    global abc_plan = ABC.abc_pmc_plan_type(EvalSysSimModel.gen_data,EvalSysSimModel.calc_summary_stats,calc_distance_ltd, param_prior, make_proposal_dist=EvalSysSimModel.make_proposal_dist_multidim_beta, is_valid=EvalSysSimModel.is_valid, normalize=EvalSysSimModel.normalize_dirch, num_part=50, num_max_attempt=50, num_max_times=200, epsilon_init=9.9e99, target_epsilon=1.0e-100, in_parallel=in_parallel, adaptive_quantiles = false, epsilon_reduction_factor=0.9, tau_factor=2.0);
+end
 
-    global abc_plan = ABC.abc_pmc_plan_type(EvalSysSimModel.gen_data,EvalSysSimModel.calc_summary_stats,calc_distance_ltd, param_prior, make_proposal_dist=ABC.make_proposal_dist_gaussian_diag_covar, is_valid=EvalSysSimModel.is_valid, normalize=EvalSysSimModel.normalize_dirch, num_part=50, num_max_attempt=50, num_max_times=200, epsilon_init=9.9e99, target_epsilon=1.0e-100, in_parallel=in_parallel, adaptive_quantiles = false, epsilon_reduction_factor=0.9, tau_factor=2.0);
-  end
-
-  function run_abc_largegen(pop::ABC.abc_population_type, ss_true::ExoplanetsSysSim.CatalogSummaryStatistics, epshist_targ::Float64, npart::Integer = 1000, num_dist::Integer = 0)
+function run_abc_largegen(pop::ABC.abc_population_type, ss_true::ExoplanetsSysSim.CatalogSummaryStatistics, epshist_targ::Float64, npart::Integer = 1000, num_dist::Integer = 0)
     sim_param_closure = setup_sim_param_christiansen()
     sim_param_closure = set_test_param(sim_param_closure)
     setup_star_table_christiansen(sim_param_closure)
     EvalSysSimModel.set_simparam_ss(sim_param_closure, ss_true)	
-
+    
     theta_true = EvalSysSimModel.get_param_vector()
     #param_prior = CompositeDist( Distributions.ContinuousDistribution[Distributions.Uniform(0., 0.3) for x in 1:length(theta_true)] )
     limitP::Array{Float64,1} = get_any(EvalSysSimModel.sim_param_closure, "p_lim_arr", Array{Float64,1})
     const r_dim = length(get_any(EvalSysSimModel.sim_param_closure, "r_lim_arr", Array{Float64,1}))-1
     prior_arr = ContinuousDistribution[]
     for i in 1:(length(limitP)-1)
-      max_in_col = 3*log(limitP[i+1]/limitP[i])/log(2)
-      lambda_col = Uniform(0.0, max_in_col)
-      dirch = Dirichlet(ones(r_dim))
-      prior_arr = vcat(prior_arr, [lambda_col, dirch])
+        max_in_col = 3*log(limitP[i+1]/limitP[i])/log(2)
+        lambda_col = Uniform(0.0, max_in_col)
+        dirch = Dirichlet(ones(r_dim))
+        prior_arr = vcat(prior_arr, [lambda_col, dirch])
     end
     param_prior = CompositeDist(prior_arr)
     in_parallel = nworkers() > 1 ? true : false
-
+    
     calc_distance_ltd(sum_stat_obs::ExoplanetsSysSim.CatalogSummaryStatistics,sum_stat_sim::ExoplanetsSysSim.CatalogSummaryStatistics) = EvalSysSimModel.calc_distance(sum_stat_obs,sum_stat_sim,num_dist)
-
-    global abc_plan = ABC.abc_pmc_plan_type(EvalSysSimModel.gen_data,EvalSysSimModel.calc_summary_stats,calc_distance_ltd, param_prior, is_valid=EvalSysSimModel.is_valid, normalize=EvalSysSimModel.normalize_dirch, num_part=npart, num_max_attempt=50, num_max_times=1, epsilon_init=9.9e99, target_epsilon=1.0e-100, in_parallel=in_parallel);
+    
+    global abc_plan = ABC.abc_pmc_plan_type(EvalSysSimModel.gen_data,EvalSysSimModel.calc_summary_stats,calc_distance_ltd, param_prior, make_proposal_dist=EvalSysSimModel.make_proposal_dist_multidim_beta, is_valid=EvalSysSimModel.is_valid, normalize=EvalSysSimModel.normalize_dirch, num_part=npart, num_max_attempt=50, num_max_times=1, epsilon_init=9.9e99, target_epsilon=1.0e-100, in_parallel=in_parallel);
 
     println("# run_abc_largegen: ",EvalSysSimModel.sim_param_closure)
     #if (std(pop.theta, 2)[1])/(mean(pop.theta, 2)[1]) > 0.3
@@ -158,30 +197,30 @@ module SysSimABC
     theta_largegen = Array{Float64}(size(pop.theta, 1), npart)
     weight_largegen = Array{Float64}(npart)
     for i in 1:npart
-      theta_val, dist_largegen, attempts_largegen = ABC.generate_theta(abc_plan, sampler_largegen, ss_true, epshist_targ)
-      theta_largegen[:,i] = theta_val  
-      prior_logpdf = Distributions.logpdf(abc_plan.prior,theta_val)
-      sampler_logpdf = ABC.logpdf(sampler_largegen, theta_val)
-      weight_largegen[i] = exp(prior_logpdf-sampler_logpdf)
+        theta_val, dist_largegen, attempts_largegen = ABC.generate_theta(abc_plan, sampler_largegen, ss_true, epshist_targ)
+        theta_largegen[:,i] = theta_val  
+        prior_logpdf = Distributions.logpdf(abc_plan.prior,theta_val)
+        sampler_logpdf = ABC.logpdf(sampler_largegen, theta_val)
+        weight_largegen[i] = exp(prior_logpdf-sampler_logpdf)
     end
     return theta_largegen, weight_largegen
-  end
+end
 
-  function run_abc(abc_plan::ABC.abc_pmc_plan_type)
+function run_abc(abc_plan::ABC.abc_pmc_plan_type)
     #global sim_param_closure
     println("# run_abc: ",EvalSysSimModel.sim_param_closure)
     ss_true = EvalSysSimModel.get_ss_obs()
     #println("True catalog SS: ", ss_true)
     pop_out = ABC.run_abc(abc_plan,ss_true;verbose=true)
-  end
+end
 
-  function run_abc(abc_plan::ABC.abc_pmc_plan_type, pop::ABC.abc_population_type)
+function run_abc(abc_plan::ABC.abc_pmc_plan_type, pop::ABC.abc_population_type)
     #global sim_param_closure
     dist_threshold = maximum(pop.dist)
     EvalSysSimModel.add_param_fixed(EvalSysSimModel.sim_param_closure,"minimum ABC dist skip pass 2",dist_threshold)
     println("# run_abc: ",EvalSysSimModel.sim_param_closure)
     ss_true = EvalSysSimModel.get_ss_obs()
     pop_out = ABC.run_abc(abc_plan,ss_true,pop;verbose=true)
-  end
+end
 
 end # module SysSimABC
