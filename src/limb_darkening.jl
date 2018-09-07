6b685ac8bb5f76ebcf1b298560b05aeedd11f593
Eric Ford <eford@psu.edu>
2018-09-07 13:41:41 -0400

    adding non-linear limb darkening for small planets

--- src/limb_darkening.jl
+++ src/limb_darkening.jl
@@ -16,7 +16,7 @@ function depth_at_midpoint(radius_ratio::Float64, ld::LimbDarkeningParamLinear)
     ksq = 1-radius_ratio^2
     tmp0 = c0/4*ksq
     tmp2 = ld.coeff[1]/6*ksq^(3//2)
-    return 1- (tmp0+tmp2)/omega
+    return  (tmp0+tmp2)/omega
 end
 
 struct LimbDarkeningParamQuadratic <: LimbDarkeningParamAbstract
@@ -32,7 +32,7 @@ function depth_at_midpoint(radius_ratio::Float64, ld::LimbDarkeningParamQuadrati
     tmp0 = c0/4*ksq
     tmp2 = (ld.coeff[1]+2*ld.coeff[2])/6*ksq^(3//2)
     tmp4 = -ld.coeff[2]/8*ksq^2
-    return 1- (tmp0+tmp2+tmp4)/omega
+    return  (tmp0+tmp2+tmp4)/omega
 end
 
 struct LimbDarkeningParam4thOrder <: LimbDarkeningParamAbstract
@@ -50,6 +50,38 @@ function depth_at_midpoint(radius_ratio::Float64, ld::LimbDarkeningParam4thOrder
     tmp2 = ld.coeff[2]/6*ksq^(3//2)
     tmp3 = ld.coeff[3]/7*ksq^(7//4)
     tmp4 = ld.coeff[4]/8*ksq^2
-    return 1- (tmp0+tmp1+tmp2+tmp3+tmp4)/omega
+    return (tmp0+tmp1+tmp2+tmp3+tmp4)/omega
 end
+
+# Cite Mandel & Agol 2002
+# See https://arxiv.org/pdf/astro-ph/0210099.pdf and https://faculty.washington.edu/agol/mandel_agol_errata.pdf
+# https://github.com/ericagol/ExoJulia/blob/master/ExoJulia/Transit/occultsmall.jl
+function depth_small_planet(z::T,p::T,ld::LimbDarkeningParam4thOrder) where T<:Real
+  @assert p<=0.2
+  @assert sum(c)<=1
+  c = ld.coeff
+
+  one_over_Omega_times_4 = 1/(1-c[1]/5-c[2]/3-3*c[3]/7-c[4]/2)
+  if z == zero(z)
+    F = p*p*one_over_Omega_times_4
+  elseif z <= 1-p   
+    sig1=sqrt(sqrt(1-(z-p)^2))
+    sig2=sqrt(sqrt(1-(z+p)^2))
+    ave_surface_intensity=(1-c[1]*(1+(sig2^5-sig1^5)/(5*p*z))-c[2]*(1+(sig2^6-sig1^6)/(6*p*z))-c[3]*(1+(sig2^7-sig1^7)/(7*p*z))-c[4]*(p^2+z^2))
+    F = p*p*ave_surface_intensity*one_over_Omega_times_4
+  elseif z<1+p
+    x=1-(z-p)^2
+    tmp=1-c[1]*(1-0.8*x^(1//4))-c[2]*(1-2//3*sqrt(x))-c[3]*(1-4//7*x^(3//4))-c[4]*(1-0.5*x)
+    F = tmp*(p^2*acos((z-1)/p)-(z-1)*sqrt(p^2-(z-1)^2))*one_over_Omega_times_4/pi
+  else
+    F = zero(T)
+  end
+  return F
+end
+
+
+
+
+
+
 
