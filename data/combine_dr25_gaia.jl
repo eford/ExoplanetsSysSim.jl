62e5532438f9f87810cde9945576fcb4fca3ccec
dch216 <dch216@psu.edu>
2018-12-11 19:10:10 -0500

    Final stellar catalog files for DR25 paper

--- data/combine_dr25_gaia.jl
+++ data/combine_dr25_gaia.jl
@@ -60,7 +60,7 @@ astrometry_good = astrometry_good .& (df[:parallax_error] .< 0.1*df[:parallax])
 
 has_mass = .! (ismissing.(df[:mass]) .| ismissing.(df[:mass_err1]) .| ismissing.(df[:mass_err2]))
 has_radius = .! (ismissing.(df[:radius]) .| ismissing.(df[:radius_err1]) .| ismissing.(df[:radius_err2]))
-has_dens = .! (ismissing.(df[:dens]) .| ismissing.(df[:dens_err1]) .| ismissing.(df[:dens_err2]))
+#has_dens = .! (ismissing.(df[:dens]) .| ismissing.(df[:dens_err1]) .| ismissing.(df[:dens_err2]))
 has_cdpp = .! (ismissing.(df[:rrmscdpp01p5]) .| ismissing.(df[:rrmscdpp02p0]) .| ismissing.(df[:rrmscdpp02p5]) .| ismissing.(df[:rrmscdpp03p0]) .| ismissing.(df[:rrmscdpp03p5]) .| ismissing.(df[:rrmscdpp04p5]) .| ismissing.(df[:rrmscdpp05p0]) .| ismissing.(df[:rrmscdpp06p0]) .| ismissing.(df[:rrmscdpp07p5]) .| ismissing.(df[:rrmscdpp09p0]) .| ismissing.(df[:rrmscdpp10p5]) .| ismissing.(df[:rrmscdpp12p0]) .| ismissing.(df[:rrmscdpp12p5]) .| ismissing.(df[:rrmscdpp15p0]))
 has_rest = .! (ismissing.(df[:dataspan]) .| ismissing.(df[:dutycycle]))
 has_limbd = .! (ismissing.(df[:limbdark_coeff1]) .| ismissing.(df[:limbdark_coeff2]) .| ismissing.(df[:limbdark_coeff3]) .| ismissing.(df[:limbdark_coeff4]))
@@ -68,12 +68,13 @@ has_limbd = .! (ismissing.(df[:limbdark_coeff1]) .| ismissing.(df[:limbdark_coef
 mast_cut =.&(df[:numLCEXqtrs].>0,df[:numLCqtrs].>4)
 
 # is_usable = .&(has_radius, has_mass, has_rest, has_cdpp, has_dens, astro_good, not_binary, planet_search, mast_cut)
-is_usable = .&(has_radius, has_mass, has_rest, has_cdpp, has_dens, mast_cut, astrometry_good, not_binary_suspect)
+is_usable = .&(has_radius, has_mass, has_rest, has_cdpp, mast_cut, astrometry_good, not_binary_suspect)
 
 df = df[find(is_usable),:]
 println("Total stars (KOIs) with valid parameters = ", length(df[:kepid]), " (", sum(df[:nkoi]),")")
 
 fgk_color = (0.5 .<= df[:bp_rp] .<= 1.7)
+# FGK = find(fgk_color)
 df = df[find(fgk_color),:]
 near_FGK_MS = []
 coeff = [2.5,-3.6,0.9]
@@ -83,7 +84,7 @@ for i in 1:6
 end
 FGK = find(near_FGK_MS)
 
-println("Total FGK stars (KOIs) with valid parameters = ", length(FGK), " (", sum(df[:nkoi]),")")
+println("Total FGK stars (KOIs) with valid parameters = ", length(FGK), " (", sum(df[FGK, :nkoi]),")")
 
 # See options at: http://exoplanetarchive.ipac.caltech.edu/docs/API_keplerstellar_columns.html
 # TODO SCI DETAIL or IMPORTANT?: Read in all CDPP's, so can interpolate?
